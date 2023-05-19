f01f0ad6fdb4204356fec40ed0e04890b2e15cf2
webMelinda <melindapinto@gmail.com>
2023-05-19 11:15:39 -0300

    cambios en comentarios
    filtrar id usando el método slice

--- js/index.js
+++ js/index.js
@@ -4,21 +4,23 @@ const API_URL = "https://jsonplaceholder.typicode.com"
 const HTMLResponse = document.querySelector("#coments")
 
 
-fetch(`${API_URL}/users`)
+fetch(`${API_URL}/comments`)
 .then(response => response.json())
 .then((users) => {
-    const tpl = users.map((user) =>` <div class="testimonial-box">
+    const selectedUsers = users.slice(0,3);
+    const tpl = selectedUsers.map((user) =>` <div class="testimonial-box">
     <div class="box-top">
         <strong>${user.name}</strong>
-        <span>${user.username}</span>                      
+        <span>${user.email}</span>                      
     </div>
     <div class="comment">
         <i class="fa-solid fa-quote-left"></i>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem adipisci hic quas optio asperiores inventore dolorum, facilis soluta tempora reiciendis voluptatum delectus possimus repellendus sequi consectetur quibusdam, laboriosam quasi quis.</p>
+        <p>${user.body}</p>
     </div>
     </div>
 
 `);
+
     HTMLResponse.innerHTML = `<div class="testimonial-box-container" id="coments">${tpl}</div>`;
 });
 
