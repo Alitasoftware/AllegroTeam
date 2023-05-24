f23493f336e95a7926bea218be09e2af4457e647
Fabrizio Lezana <Fabrizio5212@gmail.com>
2023-05-23 23:31:26 -0300

    Menu-Responsive y Formulario
    Agregué el logo del menú cuando es responsive  e hice las validaciones del formulario.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
+let menuVisible = false;
+// Mostrar-Ocultar Menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList="";
+        menuVisible = true; 
+    }
+    else{
+        document.getElementById("nav").classList="responsive";
+        menuVisible = false; 
+    }
+}
 
+//Ocultar Menu una vez seleccionado
+function seleccionar(){
+    document.getElementById("nav").classList="";
+    menuVisible = false;
+}
 const API_URL = "https://jsonplaceholder.typicode.com"
 
 const HTMLResponse = document.querySelector("#coments")
@@ -24,3 +41,34 @@ fetch(`${API_URL}/comments`)
     HTMLResponse.innerHTML = `<div class="testimonial-box-container" id="coments">${tpl}</div>`;
 });
 
+//Formulario
+function validar(){
+    var nombre, telefono, correo, asunto, mensaje, expresion;
+    nombre = document.getElementById("name").value;
+    telefono = document.getElementById("phone").value;
+    correo = document.getElementById("email").value;
+    asunto = document.getElementById("asunto").value;
+    mensaje = document.getElementById("mensaje").value;
+    expresion = /\w+@\w+\.+[a-z]/;
+
+    if(nombre === "" || telefono === "" || correo === "" || asunto === "" || mensaje === ""){
+        alert("Por favor, complete todos los campos");
+        return false;
+    }
+    else if(isNaN(telefono)){
+        alert("El teléfono ingresado no es un número");
+        return false;
+    }
+    else if(!expresion.test(correo)){
+        alert("El correo no es valido");
+        return false;
+    }
+    else if(asunto.length > 30){
+        alert("El asunto es muy largo (Max 30 caracteres)");
+        return false;
+    }
+    else if(mensaje.length > 200){
+        alert("El mensaje es muy largo (Max 200 caracteres)");
+        return false;
+    }
+}
